51c373363c51ab9749ea324d60556225a20f136f
centurion906 <32056070+centurion906@users.noreply.github.com>
2018-04-17 13:00:55 +0800

    Add files via upload

---
+++ 04-17/04-17/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+	Shape scn = new Circle(4.0);
+    }
+}
---
+++ 04-17/04-17/Measure.java
@@ -0,0 +1,4 @@
+interface Measure {
+    final double PI = 3.14;
+    abstract void area();
+}
