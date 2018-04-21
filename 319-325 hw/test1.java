d0f48b5c40c52d4f4729c2009602b7d273f6843c
centurion906 <32056070+centurion906@users.noreply.github.com>
2018-04-21 16:57:54 +0800

    Add files via upload

---
+++ 319-325 hw/test1.java
@@ -0,0 +1,26 @@
+public class test1 {
+    public String ex1, ex2, ex3;
+    public test1(String ex11, String ex21, String ex31){
+        ex1 = ex11;
+        ex2 = ex21;
+        ex3 = ex31;
+    }
+    public void setEx1(String ex12){
+        ex1 = ex12;
+    }
+    public void setEx2(String ex22){
+        ex2 = ex22;
+    }
+    public void setEx3(String ex33){
+        ex3 = ex33;
+    }
+    public String getEx1(){
+        return ex1;
+    }
+    public String getEx2(){
+        return ex2;
+    }
+    public String getEx3(){
+        return ex3;
+    }
+}
