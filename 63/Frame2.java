c8ff16048268b6f0380f04ef5e91b38b85e02e05
centurion906 <32056070+centurion906@users.noreply.github.com>
2018-06-03 22:32:46 +0800

    Add files via upload

---
+++ 63/Frame2.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Frame2 extends JFrame{
+    static int mapHeight = 9, mapWidth = 9;
+    static int frameWidth = 400, frameHeight = 400;
+    private JButton [][] jbtn;
+    private int timecount = 0;
+    private int timeContinue = 1;
+    private JLabel jlb = new JLabel("已經過時間：0");
+    private JPanel centerButtonPanel;
+    private JPanel topPanel;
+    public Frame2(){
+        ex2();
+    }public void ex2(){
+        this.setBounds(100,100,frameWidth,frameHeight);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setResizable(false);
+        this.setTitle("踩地雷");
+        jbtn = new JButton[mapHeight][mapWidth];
+        centerButtonPanel = new JPanel();
+        centerButtonPanel.setLayout(new GridLayout(mapHeight, mapWidth));
+        for (int i=0;i<mapHeight;i++){
+            for (int j=0;j<mapWidth;j++){
+                jbtn[i][j] = new JButton();
+                centerButtonPanel.add(jbtn[i][j]);
+            }
+        }
+        add(centerButtonPanel, BorderLayout.CENTER);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (timeContinue == 1) jlb.setText("已經過時間： "+(timecount++));
+            }
+        };
+        new Timer().scheduleAtFixedRate(timerTask,0,1000);
+        topPanel = new JPanel();
+        topPanel.add(jlb);
+        add(topPanel,BorderLayout.NORTH);
+    }
+}
