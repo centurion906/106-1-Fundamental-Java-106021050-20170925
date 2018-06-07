cb9e0fa2650033b90f2fd0ac18ed09b5c6839fd4
centurion906 <32056070+centurion906@users.noreply.github.com>
2018-06-07 22:04:34 +0800

    Add files via upload

--- 63/Frame2.java
+++ 63/Frame2.java
@@ -1,28 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Frame2 extends JFrame{
-    static int mapHeight , mapWidth;
+    static int mapHeight = 9, mapWidth = 9;
     static int frameWidth = 400, frameHeight = 400;
     private JButton [][] jbtn;
+    private int bombCount = 10;
+    private JLabel bombmany = new JLabel("目前炸彈數："+bombCount);
+    private boolean buttonpull [][] = new boolean[mapHeight][mapWidth];
     private int timecount = 0;
     private int timeContinue = 1;
     private JLabel jlb = new JLabel("已經過時間：0");
     private JPanel centerButtonPanel;
     private JPanel topPanel;
-
     public Frame2(int v1,int v2){
         mapHeight = v1;
         mapWidth = v2;
         ex2();
-    }public void ex2(){
+    }
+    public void ex2(){
         this.setBounds(100,100,frameWidth,frameHeight);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.setResizable(false);
         this.setTitle("踩地雷");
+//        ----------------建立地圖-------------------------
         jbtn = new JButton[mapHeight][mapWidth];
         centerButtonPanel = new JPanel();
         centerButtonPanel.setLayout(new GridLayout(mapHeight, mapWidth));
@@ -32,16 +39,52 @@ public class Frame2 extends JFrame{
                 centerButtonPanel.add(jbtn[i][j]);
             }
         }
+//        ------------------上面介面------------------------------
         add(centerButtonPanel, BorderLayout.CENTER);
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                if (timeContinue == 1) jlb.setText("已經過時間： "+(timecount++));
+                if (timeContinue == 1) jlb.setText("已經過："+(timecount++)+"秒");
             }
         };
         new Timer().scheduleAtFixedRate(timerTask,0,1000);
         topPanel = new JPanel();
         topPanel.add(jlb);
+        bombmany.setText("目前炸彈有："+bombCount);
+        JButton restart = new JButton("新局");
+        restart.setActionCommand("");
         add(topPanel,BorderLayout.NORTH);
+        topPanel.add(restart);
+        topPanel.add(bombmany);
+//        ---------------------按鈕設定------------------------------
+        JPanel centerButtonPanel = new JPanel();
+        centerButtonPanel.setLayout(new GridLayout(mapHeight,mapWidth));
+        for (int i = 0;i<mapHeight;i++){
+            for (int j = 0;j<mapWidth;j++){
+                jbtn [i][j] = new JButton();
+                jbtn [i][j].setBackground(Color.WHITE);
+                jbtn [i][j].setActionCommand(i+""+j);
+//                jbtn [i][j].addActionListener(this);
+                centerButtonPanel.add(jbtn[i][j]);
+            }
+        }add(centerButtonPanel,BorderLayout.CENTER);
+//        ---------------新局----------------------------------
+        restart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timecount = 1;
+                timeContinue = 1;
+                //for (int i = 0;i<mapHeight;i++) Arrays.fill(jbtn[i],0);//*****************
+                for (int i = 0;i<mapHeight;i++) Arrays.fill(buttonpull[i],false);
+                for (int i = 0;i<mapHeight;i++){
+                    for (int j = 0;j<mapWidth;j++){
+                        jbtn[i][j].setBackground(Color.WHITE);
+                        jbtn[i][j].setText("");
+                    }
+                }
+                bombCount = 10;
+                bombmany.setText("目前炸彈數："+bombCount);
+            }
+        });
     }
 }
